b4ec8f0f7b67ce722336063ee3a01dec9b04dc64
VsimamuraV <89236595+VsimamuraV@users.noreply.github.com>
2021-08-20 13:28:10 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ from asyncio import sleep
 import requests
 import datetime
 
-TOKEN = os.environ.get('DISCORD_TOKEN')
-VC_ID = os.environ.get('VOICECHAT_ID')
+TOKEN = os.environ.get('ODc4MTE1Nzk5MjQ2NzI5MjU3.YR8fEQ.PvNzKUre2d-VfM5uYsFlYs4eL3E')
+VC_ID = os.environ.get('730865286810435689')
 
 def spla2API(rule):
     match = rule
